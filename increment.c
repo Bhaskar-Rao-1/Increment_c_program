bfe7b01e06007a92817d10ef1f3700c3eb298721
Bhaskar-Rao-1 <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-07-30 17:07:03 +0530

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+     int i=1;
+    printf("%d\n",i++);
+    printf("%d\n",i);
+    
+    printf("%d\n",++i);
+    printf("%d\n",i);
+    
+    
+    printf("%d\n",i--);
+    printf("%d\n",i);
+    
+    printf("%d\n",--i);
+    printf("%d",i);
+    return 0;
+}
